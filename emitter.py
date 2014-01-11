4a2e2e593fbaf81808ab5130e51ee1458fbdde2c
Tomasz Pewiński <pewniak747@gmail.com>
2014-01-11 18:49:40 +0100

    keyboard event emitter

---
+++ emitter.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python
+
+from pykeyboard import PyKeyboard
+import sys
+
+k = PyKeyboard()
+
+keys = {
+  'left': 'a',
+  'right': 'd',
+  'accelerate': 'w',
+  'brake': 's',
+  'fire': 'space',
+  'nitro': 'n'
+}
+
+for line in sys.stdin:
+  line = line.strip()
+  pair = line.split("-")
+  if len(pair) != 2:
+    print("unknown event: " + line)
+  else:
+    print("EVENT: " + line)
+    if pair[0] in keys:
+      key = keys[pair[0]]
+      if pair[1] == "up":
+        k.release_key(key)
+      elif pair[1] == "down":
+        k.press_key(key)
+      elif pair[1] == "press":
+        k.tap_key(key)
+      else:
+        print("unknown event: " + line)
+    else:
+      print("unknown event: " + line)
